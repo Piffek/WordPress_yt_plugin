e988e9e677a931c6deab0f597284b5cc3238e8dc
Piffek <patrykpiwko123412@gmail.com>
2017-08-27 19:29:10 +0200

    you can change channel in admin panel

--- wp-content/plugins/youtubesubs/includes/youtubesubs-scripts.php
+++ wp-content/plugins/youtubesubs/includes/youtubesubs-scripts.php
@@ -5,6 +5,10 @@ function yts_add_scripts(){
     wp_enqueue_style('yts-main-style', plugins_url(). '/youtubesubs/css/style.css');
     //JS
     wp_enqueue_script('yts-main-script', plugins_url(). '/youtubesubs/js/main.js');
+    
+    wp_register_script('google', 'https://apis.google.com/js/platform.js');
+    
+    wp_enqueue_script('google');
 }
 
 add_action('wp_enqueue_scripts', 'yts_add_scripts');
--- wp-content/plugins/youtubesubs/includes/youtubesubs_class.php
+++ wp-content/plugins/youtubesubs/includes/youtubesubs_class.php
@@ -30,7 +30,7 @@ class YouTube_Subs_Widget extends WP_Widget {
         if ( ! empty( $instance['title'] ) ) {
             echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
         }
-        echo 'Hello, World!dfsf';
+        echo '<div class="g-ytsubscribe" data-channel="'.$instance['channel'].'" data-layout="full" data-count="default"></div>';
         
         echo $args['after_widget'];
     }
@@ -44,6 +44,8 @@ class YouTube_Subs_Widget extends WP_Widget {
      */
     public function form( $instance ) {
         $title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'Subs YT', 'yts_domain' );
+        
+        $channel = ! empty( $instance['channel'] ) ? $instance['channel'] : esc_html__( 'PatrykPiwko', 'yts_channel' );
         ?>
         
 		<p>
@@ -57,6 +59,19 @@ class YouTube_Subs_Widget extends WP_Widget {
 		name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" 
 		type="text" 
 		value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		
+		<p>
+		<label for="<?php echo esc_attr( $this->get_field_id( 'channel' ) ); ?>">
+		    <?php esc_attr_e( 'Channel:', 'yts_channel' ); ?>
+		</label> 
+		
+		<input 
+		class="widefat" 
+		id="<?php echo esc_attr( $this->get_field_id( 'channel' ) ); ?>" 
+		name="<?php echo esc_attr( $this->get_field_name( 'channel' ) ); ?>" 
+		type="text" 
+		value="<?php echo esc_attr( $channel ); ?>">
 		</p>
 		
 		<?php 
@@ -75,6 +90,8 @@ class YouTube_Subs_Widget extends WP_Widget {
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		
+		$instance['channel'] = ( ! empty( $new_instance['channel'] ) ) ? strip_tags( $new_instance['channel'] ) : '';
 
 		return $instance;
 	}
